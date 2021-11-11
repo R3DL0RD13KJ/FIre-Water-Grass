015f8a32c6613e8cc7aa4c01aea0a040a4837468
Kenneth Johnson <kd123johnson@gmail.com>
2021-11-11 09:29:29 -0700

    Initial Commit

---
+++ FIre,Water,Grass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIre,Water,Grass
+//
+//  Created by Kenneth Johnson on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
