81fc696b5f6ea5e276a32820bafc57784ea25ff0
Kenneth Johnson <kd123johnson@gmail.com>
2021-11-16 10:42:35 -0700

    MVP 3

--- FIre,Water,Grass/ViewController.swift
+++ FIre,Water,Grass/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController
     
     @IBOutlet weak var yourElementChoice: UILabel!
     
-    @IBOutlet var elementPick: [UILabel]!
+    @IBOutlet var optionalPicks: [UIImageView]!
     
     @IBOutlet weak var elementChoices: UIStackView!
     
@@ -23,24 +23,37 @@ class ViewController: UIViewController
     var elementWater = "Water"
     var elementGrass = "Grass"
     
-    var counter = 0
+    var userPicks = 0
+    var AIpicks = 0
     
-    
+    var images:[String] = []
     
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let fireImage = String(named: "Fire")
+        let grassImage = String(named: "Grass")
+        let waterImage = String(named: "Water")
+        
+        let images = [fireImage, grassImage, waterImage]
+        //as! [UIImage]
     }
 
     @IBAction func whenTapped(_ sender: Any)
     {
         
+        let seleectedpoint = (sender as AnyObject).location(in: elementChoices)
+        for image in optionalPicks
+        {
+            if image.frame.contains(seleectedpoint)
+            {
+                userPicks = view.tag
+                yourElementChoice
+            }
+        }
     }
     
     
-    
-
 }
-
